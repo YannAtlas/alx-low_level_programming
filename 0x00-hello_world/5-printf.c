481a56e4a05215bb3318ac57f92671a0cf2b8862
YannAtlas <tchapda.yann@2027.icam.fr>
2023-03-16 18:38:12 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints line with printf function
+ * Return: 0 (SUCCESS)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
