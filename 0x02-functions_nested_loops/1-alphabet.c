3be7872a47f6d0455b32c5b73ae4db7d684f3790
YannAtlas <tchapda.yann@2027.icam.fr>
2023-03-22 10:54:52 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,9 @@
 
 void print_alphabet(void)
 {
-	char ;
+	char letter ;
 	
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	_putchar("\n");
 }
