4206bb7782cdc5af986bfda535d0f482eae5e636
YannAtlas <tchapda.yann@2027.icam.fr>
2023-03-23 13:38:20 -0700

    test

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,13 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n')
+		_putchar('\n');
 	}
 	else
 	{
 		for (x = 0; x < size; ++x)
 		{
-			for (y = size - x; y > 1, --y)
+			for (y = size - x; y > 1; --y)
 			{
 				_putchar(32);
 			}
@@ -27,7 +27,7 @@ void print_triangle(int size)
 			{
 				_putchar(35);
 			}
-			_putchar();
+			_putchar('\n');
 		}
 	}
 }
