b0e148637c3f3b069a23bbd7e12bba7431a2d44d
YannAtlas <tchapda.yann@2027.icam.fr>
2023-03-23 07:38:15 -0700

    test 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * do not print 2 and 4
+ * followed by a new line
+ */
+
+void print_most_numbers(void);
+{
+	int n = 0;
+
+	while (i <= 9)
+	{
+		while (i != 2 && i != 4)
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+
+void more_numbers(void);
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
